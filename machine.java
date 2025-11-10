b901fd74d6d287a0958fb8030630baff75d94642
Hamadellah <hamadellahotman13@gmail.com>
2025-11-10 09:47:31 +0100

    adding a fil

---
+++ machine.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class machine {
+    static ArrayList<String> produits = new ArrayList<>(List.of("Eau", "Soda", "Chips", "Chocolat"));
+    static ArrayList<Double> prix = new ArrayList<>(List.of(5.0, 8.0, 12.0, 15.0));
+    static ArrayList<Integer> stock = new ArrayList<>(List.of(10, 5, 7, 3));
+    static void afficherProduits() {
+        System.out.println("Produits disponibles :");
+        for (int i = 0; i < produits.size(); i++) {
+            System.out.println((i + 1) + ". " + produits.get(i) + " (" + prix.get(i) + " MAD) - Stock : " + stock.get(i));
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choix;
+
+        do {
+            System.out.println("=== Distributeur Automatique ===");
+            System.out.println("1. Afficher les produits");
+            System.out.println("2. Acheter un produit");
+            System.out.println("3. Quitter");
+            System.out.print("Choix : ");
+            choix = scanner.nextInt();
+
+            switch (choix) {
+                case 1:
+                    afficherProduits();
+                    break;
+                case 2:
+
+                case 3:
+                    System.out.println("Au revoir !");
+                    break;
+                default:
+                    System.out.println("Choix invalide. Veuillez réessayer.");
+            }
+        } while (choix != 3);
+
+        scanner.close();
+    }
+}
